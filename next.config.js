6703795fa2e5ba78295929c9fc7bd5c1e7eb3f8c
Trezy <t@trezy.com>
2022-06-26 10:22:47 -0500

    add CoC and Discord redirects

--- next.config.js
+++ next.config.js
@@ -10,6 +10,16 @@ module.exports = {
 				destination: 'https://robertsspaceindustries.com/orgs/QLOG',
 				permanent: false,
 			},
+			{
+				source: '/code-of-conduct',
+				destination: 'https://drive.google.com/file/d/1Pp1IEf0En0a2-AjFUO-bF2O0PR8usLvo/view',
+				permanent: false,
+			},
+			{
+				source: '/discord',
+				destination: 'https://discord.gg/ZufmY94NKN',
+				permanent: true,
+			},
 		]
 	},
 }
